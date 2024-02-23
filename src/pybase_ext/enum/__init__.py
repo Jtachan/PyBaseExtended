38a337cf28d1ba924edbb8ca216b2629b78ce655
Jtachan <76098522+Jtachan@users.noreply.github.com>
2024-02-23 20:17:12 +0100

    Missing enum type

--- src/pybase_ext/enum/__init__.py
+++ src/pybase_ext/enum/__init__.py
@@ -1,7 +1,16 @@
 """
 Different enumerations types supported for python >= 3.8
 """
-__all__ = ["StrEnum", "TupleEnum", "auto", "IntEnum", "IntFlag", "Flag", "unique"]
-from enum import Flag, IntEnum, IntFlag, auto, unique
+__all__ = [
+    "Enum",
+    "Flag",
+    "IntEnum",
+    "IntFlag",
+    "auto",
+    "unique",
+    "StrEnum",
+    "TupleEnum",
+]
+from enum import Enum, Flag, IntEnum, IntFlag, auto, unique
 
 from ._types import StrEnum, TupleEnum
