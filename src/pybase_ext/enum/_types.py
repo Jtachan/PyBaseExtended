8cf12c8ae4dfa9ab0932a074da7823afc4d6b13c
Jtachan <76098522+Jtachan@users.noreply.github.com>
2024-02-23 20:17:20 +0100

    Using f-strings

--- src/pybase_ext/enum/_types.py
+++ src/pybase_ext/enum/_types.py
@@ -17,21 +17,22 @@ class StrEnum(builtins.str, ReprEnum):
     """Enum where members are also (and must be) strings."""
 
     def __new__(cls, *values):
-        """values must already be of type `str`."""
+        """Method copied from original enum.StrEnum code.
+        Values must already be of type `str`."""
         if len(values) > 3:
-            raise TypeError('too many arguments for str(): %r' % (values, ))
+            raise TypeError(f"Too many arguments for str(): {values}")
         if len(values) == 1:
             # it must be a string
             if not isinstance(values[0], str):
-                raise TypeError('%r is not a string' % (values[0], ))
+                raise TypeError(f"{values[0]} is not a string")
         if len(values) >= 2:
             # check that encoding argument is a string
             if not isinstance(values[1], str):
-                raise TypeError('encoding must be a string, not %r' % (values[1], ))
+                raise TypeError(f"Encoding must be a string, not {values[1]}")
         if len(values) == 3:
             # check that errors argument is a string
             if not isinstance(values[2], str):
-                raise TypeError('errors must be a string, not %r' % (values[2]))
+                raise TypeError(f"Errors must be a string, not {values[2]}")
         value = str(*values)
         member = str.__new__(cls, value)
         member._value_ = value
